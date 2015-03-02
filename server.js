1eb64d15c605359872fabcd49f5bbee648a3c0aa
joejoe973 <joejoe973@hotmail.com>
2015-03-02 16:50:09 +0800

    web I'm coming

--- server.js
+++ server.js
@@ -51,9 +51,11 @@ var server = http.createServer(function(request, response) {
   var absPath = './' + filePath;
   serveStatic(response, cache, absPath);
 });
-
-server.listen(3000, function() {
-  console.log("Server listening on port 3000.");
+	
+	port = process.env.PORT || 5000 ;  
+ 
+server.listen(port, function() {
+  console.log("Server listening on port 5000.");
 });
 
 var chatServer = require('./lib/chat_server');
